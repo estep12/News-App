9a4602d4e8c4260f41bddad14418433bd226dd23
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-06-21 21:42:23 -0500

    Added buttons to articles

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,10 @@
 $.getJSON("/articles", (data) => {
     for(i = 0; i< data.length; i++){
-        $("#card-body").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        $("#card-body").append("<p data-id='" 
+        + data[i]._id + "'>" 
+        + data[i].title + "<br />" 
+        + data[i].link + 
+        "<button class = 'button button-success'> Save </button>" +
+        "</p>");
     }
 });
